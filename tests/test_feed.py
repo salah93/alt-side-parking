1a385cca99a04c7f0ca1958c6a5bcc39c9a38359
Salah Ahmed <salahs.email@pm.me>
2020-09-20 22:56:55 -0400

    add tests

---
+++ tests/test_feed.py
@@ -0,0 +1,26 @@
+from automock import swap_mock
+from alt_side_parking.feed import get_nyc_alt_side_parking_tweets, is_suspended_today
+
+
+@swap_mock("alt_side_parking.feed.get_client", failed_response=True)
+def test_failed_fetch():
+    tweets = get_nyc_alt_side_parking_tweets()
+    assert [] == tweets
+
+
+def test_get_tweets():
+    tweets = get_nyc_alt_side_parking_tweets()
+    assert [
+        "#NYCASP rules will be in effect tomorrow, Monday, September 21",
+        "#NYCASP rules will be suspended today, September 20",
+    ] == tweets
+
+
+def test_is_suspended_failed(freezer):
+    freezer.move_to('2020-09-23')
+    assert not is_suspended_today()
+
+
+def test_is_suspended_success(freezer):
+    freezer.move_to('2020-09-20')
+    assert is_suspended_today()
