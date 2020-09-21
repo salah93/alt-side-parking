33b092d12e25828456337307b3899f9f97f0da16
Salah Ahmed <salahs.email@pm.me>
2020-09-20 22:58:19 -0400

    use utf-8

--- src/alt_side_parking/feed.py
+++ src/alt_side_parking/feed.py
@@ -41,7 +41,7 @@ def get_nyc_alt_side_parking_tweets(count=5) -> List[str]:
     url = TWEETS_API_URL + urlencode(search_query)
     client = get_client()
     response, tweets = client.request(
-        url.encode("ascii"),
+        url.encode("utf-8"),
         method="GET",
         body="".encode("utf-8"),
         headers=None,
