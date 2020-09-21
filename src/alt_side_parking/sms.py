9bde6c1aac6dc8d314964719d96929fde263e7cf
Salah Ahmed <salahs.email@pm.me>
2020-09-20 21:54:06 -0400

    add sms module for sending texts

---
+++ src/alt_side_parking/sms.py
@@ -0,0 +1,31 @@
+import phonenumbers
+from twilio.rest import Client
+from .config import (
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN,
+    TWILIO_NUMBER,
+    MOBILE,
+    REMINDER_TEXT,
+)
+
+
+def send_reminder():
+    client = get_client()
+    personal_number = normalize_number(MOBILE)
+    twilio_number = normalize_number(TWILIO_NUMBER)
+    client.api.account.messages.create(
+        to=personal_number,
+        from_=twilio_number,
+        body=REMINDER_TEXT,
+    )
+
+
+def get_client():
+    return Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+def normalize_number(mobile_number: str):
+    return phonenumbers.format_number(
+        phonenumbers.parse(mobile_number, None),
+        phonenumbers.PhoneNumberFormat.E164,
+    )
